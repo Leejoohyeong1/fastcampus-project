e219e149e6eea38ea82f55cfe9a95a3f74c9d42f
LEE <lee@LEEui-MacBook-Pro.local>
2018-03-13 14:17:21 +0900

    1

--- FoodFly/ViewController.swift
+++ FoodFly/ViewController.swift
@@ -8,7 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController,UICollectionViewDelegateFlowLayout,UICollectionViewDataSource {
+class ViewController: UIViewController,UICollectionViewDelegateFlowLayout,UICollectionViewDataSource,UIScrollViewDelegate{
+
+    
     @IBOutlet weak var cataLogTitle: UICollectionView!
     
     @IBOutlet weak var menuListScroll: UIScrollView!
@@ -18,50 +20,19 @@ class ViewController: UIViewController,UICollectionViewDelegateFlowLayout,UIColl
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
-        
-        
-        
-      
-
-
-//        self.menuListScroll.addSubview(vcView1)
-        
-        
-        
-        
+        self.menuListScroll.delegate = self
         cataLogTitle.delegate = self
         cataLogTitle.dataSource = self
         self.cataLogTitle.showsHorizontalScrollIndicator = false
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-    
-    
-    
-    
-//    override func layoutIfNeeded() {
-//        super.layoutIfNeeded()
-//
-//    }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         
         self.menuListScroll.isPagingEnabled = true
         self.menuListScroll.contentSize = CGSize(width: self.menuListScroll.bounds.width * CGFloat(label.count), height: self.menuListScroll.frame.height)
-    
-        
-//        let btn:UIButton = UIButton(frame: self.menuListScroll.frame)
-//
-//        btn.backgroundColor = UIColor.black
-//        self.view.addSubview(btn)
-        
-        
-        
-        
-        
-        
+
         
         for (index,content) in self.label.enumerated() {
             let testVC1 = self.storyboard?.instantiateViewController(withIdentifier: "sub") as! subViewController
@@ -72,31 +43,8 @@ class ViewController: UIViewController,UICollectionViewDelegateFlowLayout,UIColl
             self.menuListScroll.addSubview(testVC1.view)
             testVC1.willMove(toParentViewController: self)
         }
-       
-        
-
-        
-       
-        
-        
-
-        
-   
-    
-
-        
-        
-        
-        
-        
-//        vcView1.addSubview(testVC1.view)
-//
-//        self.menuListScroll.addSubview(vcView1)
-        
     }
     
-    
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -113,14 +61,16 @@ class ViewController: UIViewController,UICollectionViewDelegateFlowLayout,UIColl
         return cell
     }
 
-    
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        print(label[indexPath.row])
-        
-        
         let x = CGFloat(indexPath.row) * menuListScroll.frame.size.width
         menuListScroll.setContentOffset(CGPoint(x: x,y :0), animated: true)
     }
-
+    
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        let position = scrollView.contentOffset.x / view.frame.size.width
+        let indexPath = IndexPath(item: Int(position), section: 0)
+        cataLogTitle.scrollToItem(at: indexPath, at: .left, animated: true)
+    }
 }
 
